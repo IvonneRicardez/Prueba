34d2cc01787de543fa040bdf374df043d4298b36
Ivonne Ricardez <51245477+IvonneRicardez@users.noreply.github.com>
2026-05-15 10:52:03 -0600

    referenciado

--- segundo.php
+++ segundo.php
@@ -1,5 +1,17 @@
 <?php
-  // Este es un comentario
-  echo "¡Hola Mundo!"; 
-?>
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hola
+    <a href="Index.php">
+        Ir a Inicio
+    </a>
+</body>
+</html>
